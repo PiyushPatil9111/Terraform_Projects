67d8db9a3221a404b5a243d1e3e2ced2afdbabe7
PiyushUser1 <piyushpatil911@gmail.com>
2025-06-08 19:38:47 +0100

    changes made

--- terraform-project/environments/dev/main.tf
+++ terraform-project/environments/dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc" {
-    source              = "../modules/vpc"
+    source              = "../..modules/vpc"
     cidr_block          = var.cidr_block
     region              = var.region
     public_subnet_cidrs = var.public_subnet_cidrs
